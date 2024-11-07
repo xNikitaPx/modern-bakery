6f88f815c9a6027da5a840fed489de18e83519b9
Nikita Popov <b.boynikone@gmail.com>
2024-11-07 02:12:35 +0200

    Finish website

--- js/modal.js
+++ js/modal.js
@@ -1,7 +1,6 @@
 const modal = document.querySelector(".backdrop");
 const modalBtnOpen = document.querySelector(".modal-btn-open");
 const modalBtnClose = document.querySelector(".modal-btn-close");
-
 const toggleModal = () => modal.classList.toggle("is-hidden");
 
 modalBtnOpen.addEventListener("click", toggleModal);
