7a869f9953c2a2d71d7e909c3cce51f8d5b76998
Nikita Popov <b.boynikone@gmail.com>
2024-12-20 03:02:39 +0200

    add second button active

--- js/modal.js
+++ js/modal.js
@@ -1,7 +1,9 @@
 const modal = document.querySelector(".backdrop");
 const modalBtnOpen = document.querySelector(".modal-btn-open");
+const modalButtonOpen = document.querySelector(".modal-button-open");
 const modalBtnClose = document.querySelector(".modal-btn-close");
 const toggleModal = () => modal.classList.toggle("is-hidden");
 
 modalBtnOpen.addEventListener("click", toggleModal);
+modalButtonOpen.addEventListener("click", toggleModal);
 modalBtnClose.addEventListener("click", toggleModal);
